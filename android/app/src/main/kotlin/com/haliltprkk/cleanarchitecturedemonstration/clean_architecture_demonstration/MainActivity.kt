cbdda632b9c1d62f120641e947b3f180c7d608cf
haliltprkk <haliltprkk@gmail.com>
2022-11-06 18:16:02 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/haliltprkk/cleanarchitecturedemonstration/clean_architecture_demonstration/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.haliltprkk.cleanarchitecturedemonstration.clean_architecture_demonstration
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
